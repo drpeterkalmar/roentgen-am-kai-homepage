896b6e8b40dc0dd420667a3d978d39ec569eb5f8
Peter Kalmar <dr.peter.kalmar@gmail.com>
2026-04-19 17:03:09 +0200

    perf: improve mobile LCP and FCP via App Shell and lazy loading components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import Hero from '../components/Hero';
-import Services from '../components/Services';
-import About from '../components/About';
 import Appointment from '../components/Appointment';
-import Blog from '../components/Blog';
+
+const Services = lazy(() => import('../components/Services'));
+const About = lazy(() => import('../components/About'));
+const Blog = lazy(() => import('../components/Blog'));
 
 const Home = () => {
   return (
     <>
       <Hero />
-      <Services />
-      <About />
+      <Suspense fallback={<div className="min-h-screen bg-transparent" />}>
+        <Services />
+        <About />
+      </Suspense>
       <Appointment />
-      <Blog />
+      <Suspense fallback={<div className="min-h-[500px] bg-transparent" />}>
+        <Blog />
+      </Suspense>
     </>
   );
 };
