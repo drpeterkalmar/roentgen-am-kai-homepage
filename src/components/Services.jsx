1e83bc020db2cbb34ed84d55986ad48d54f9f45c
Peter Kalmar <dr.peter.kalmar@gmail.com>
2026-04-19 20:38:44 +0200

    Update DVT service bento icon to a tooth

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -9,6 +9,13 @@ const ChevronRight = ({ size }) => (
   </svg>
 );
 
+const ToothIcon = ({ size }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <path d="M20 11c0-4-2.5-7-5-7-1.5 0-3 1-3 1s-1.5-1-3-1c-2.5 0-5 3-5 7 0 5 2 9 4 9 1 0 2-1 2-2v-2.5a2.5 2.5 0 0 1 5 0V18c0 1 1 2 2 2 2 0 4-4 4-9z" />
+  </svg>
+);
+
+
 const services = [
   {
     title: 'Mammographie',
@@ -62,7 +69,7 @@ const services = [
     description: 'Präzise 3D-Kieferdiagnostik für die Implantatplanung.',
     microInfo: 'Modernste Technik',
     href: '/unser-angebot/dvt',
-    icon: <Box size={24} />,
+    icon: <ToothIcon size={24} />,
     color: 'bg-orange-50 text-orange-600',
     gridClass: 'md:col-span-4 aspect-square',
     image: '/assets/images/hero_interior.avif'
