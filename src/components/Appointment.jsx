5d765a5762d9fd30be66e13b182f82c7ee20323e
Peter Kalmar <dr.peter.kalmar@gmail.com>
2026-04-20 22:23:09 +0200

    feat: update frontend script URL to latest deployment

--- src/components/Appointment.jsx
+++ src/components/Appointment.jsx
@@ -57,7 +57,7 @@ const Appointment = () => {
     setSubmitError(null);
 
     // Google Apps Script Integration (Unlimited & Free)
-    const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyq2kxFwaTP4RVbbQgFbU_GQ0jvfsXsFrf9iwwnFWjc3fGxITwSJ6Fbvyp_dJKwUBDI_w/exec";
+    const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby0LAjUY_zUhc_sm2hw_W6VesyL1ErsOYEFBAA0aXGDxjPYxnbLaHzrTpygWK3uZsYHyw/exec";
     
     // Metadata fields for Google Sheet/Email notification
     const fullName = `${formData.firstName} ${formData.lastName}`;
